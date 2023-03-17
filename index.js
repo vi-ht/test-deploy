edb716ab6fed6bb5cb02b99103a36b54965c7129
vi-ht <74223356+vi-ht@users.noreply.github.com>
2023-03-17 23:10:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
   res.json({ name: "vi" });
 });
 app.get("/data", (req, res) => {
-  res.jsonp({ name: "thanhvi", age: 23 });
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ name: "thanhvi", age: 23 }));
 });
 
 app.listen(port, () => {
