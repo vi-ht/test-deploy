4bf335d5123c1d1bede027fc77955fa6db01e9bd
vi-ht <74223356+vi-ht@users.noreply.github.com>
2023-03-18 00:00:08 +0700

    add config

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const port = 3000;
-
+var corsOptions = {
+  origin: '*',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.json({ name: "vi" });
 });
-app.get("/data", (req, res) => {
+app.get("/data", cors(corsOptions),(req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ name: "thanhvi", age: 23 }));
 });
