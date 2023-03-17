ecc5447d6d9ea54e3396f0ec78ac2f492e389bda
vi-ht <74223356+vi-ht@users.noreply.github.com>
2023-03-17 22:48:12 +0700

    add path

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.json({"name": 'vi'})
-})
+app.get("/", (req, res) => {
+  res.json({ name: "vi" });
+});
+app.get("/data", (req, res) => {
+  res.jsonp({ name: "thanhvi", age: 23 });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
